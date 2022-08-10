af728ea3324971c4daea6fc368fd33b3f4e11fcb
deepak dhaked <66664604+deepakdhaked@users.noreply.github.com>
2022-08-10 16:26:39 +0530

    Create print_hello_n_times.cpp

---
+++ print_hello_n_times.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void fun(int n)
+{
+    // Time Complexity : O(n)
+    // Space Complexity : O(n)
+    if (n == 0)
+    {
+        return;
+    }
+    cout << "hello world\n";
+    fun(n - 1);
+}
+
+void fun(int i, int n)
+{
+    // Time Complexity : O(n)
+    // Space Complexity : O(n)
+    if (i > n)
+    {
+        return;
+    }
+    cout << "hello world\n";
+    fun(i + 1, n);
+}
+
+int main()
+{
+    int n;
+    n = 5;
+    fun(n);
+    cout << "-----------------------------\n";
+    fun(1, n);
+}
